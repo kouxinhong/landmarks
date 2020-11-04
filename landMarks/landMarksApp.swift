0f6dece228c2663135fef3c3dbc9464c8c29c91e
kxh <kouxinhong@apkpure.net>
2020-11-04 11:05:16 +0800

    Initial Commit

---
+++ landMarks/landMarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  landMarksApp.swift
+//  landMarks
+//
+//  Created by kxh on 2020/11/4.
+//
+
+import SwiftUI
+
+@main
+struct landMarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

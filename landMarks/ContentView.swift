8110d233cb5310e91c20b83db59b0d1972c6df60
kxh <kouxinhong@apkpure.net>
2020-11-04 14:18:15 +0800

    first commit

--- landMarks/ContentView.swift
+++ landMarks/ContentView.swift
@@ -9,13 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundColor(.black)
+                    .lineLimit(0)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                }
+            }
             .padding()
+            Spacer()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
